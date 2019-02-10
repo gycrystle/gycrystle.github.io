255d0d11e68c639cfa44ef75a0e13075927edc45
Greace CRYSTLE <greaceyc@hotmail.com>
2019-02-10 19:26:34 +0100

    sprinkle me with stars

--- js/index.js
+++ js/index.js
@@ -33,7 +33,7 @@ $.init = function () {
    ========================================*/
 
 $.reset = function () {
-	$.w = window.innerWidth*0.95;
+	$.w = window.innerWidth*0.98;
 	$.h = window.innerHeight*0.15;
 	$.cx = $.w / 2;
 	$.cy = $.h / 2;
